541f8f2f4c3ecc73e323264e2fe27c9db292560c
Jack Ashwell <jack.ashwell1@gmail.com>
2026-01-04 18:32:17 +0000

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+"""Manages the Conan configuration for the application."""
+
+from __future__ import annotations
+
+# Builtin
+from typing import TYPE_CHECKING
+
+# Pip
+from conan import ConanFile
+
+if TYPE_CHECKING:
+    from typing import ClassVar
+
+
+class Exodus(ConanFile):  # type: ignore[misc]
+    """The Conan configuration for the application."""
+
+    # The metadata for the package
+    generators: ClassVar[list[str]] = ["CMakeDeps"]
+    settings: ClassVar[list[str]] = ["build_type"]
+
+    def requirements(self: ConanFile) -> None:
+        """Add the required dependencies to the Conan configuration."""
